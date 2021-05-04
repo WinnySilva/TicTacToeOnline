ee6785a462a6f2d92d4381917d362f9a104f9c88
Wine Silva <winny.silva@yahoo.com.br>
2021-05-04 19:22:31 -0300

    controle da conexão

---
+++ Assets/Script/ConexaoController.cs
@@ -0,0 +1,24 @@
+﻿using MLAPI;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    public class ConexaoController : MonoBehaviour
+    {
+
+        public TMPro.TMP_InputField serverAddress;
+
+        public void IniciarHost()
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+
+        public void IniciarClient()
+        {
+            var network = NetworkManager.Singleton;
+           
+            NetworkManager.Singleton.StartClient();
+        }
+
+    }
+}
